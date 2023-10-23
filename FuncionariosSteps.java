ea4c0a00db968edc05db925bba6ac61c41bf80fc
ArthurRibeiro02 <103151964+ArthurRibeiro02@users.noreply.github.com>
2023-10-23 20:38:08 -0300

    Create FuncionariosSteps.java

---
+++ FuncionariosSteps.java
@@ -0,0 +1,32 @@
+package SeuPacote;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Quando;
+import io.cucumber.java.pt.Entao;
+
+public class SeusTestesSteps {
+    @Dado("que estou logado no sistema")
+    public void que_estou_logado_no_sistema() {
+        // Implemente o código para fazer login no sistema
+    }
+
+    @Quando("eu adiciono um novo livro com os seguintes detalhes:")
+    public void eu_adiciono_um_novo_livro_com_os_seguintes_detalhes(DataTable dataTable) {
+        // Implemente o código para adicionar um novo livro
+    }
+
+    @Entao("o livro é adicionado com sucesso")
+    public void o_livro_e_adicionado_com_sucesso() {
+        // Implemente as verificações para garantir que o livro foi adicionado com sucesso
+    }
+
+    @Quando("eu pesquiso um livro por título {string}")
+    public void eu_pesquiso_um_livro_por_titulo(String titulo) {
+        // Implemente o código para pesquisar um livro por título
+    }
+
+    @Entao("vejo os detalhes do livro na lista de resultados")
+    public void vejo_os_detalhes_do_livro_na_lista_de_resultados() {
+        // Implemente as verificações para garantir que os detalhes do livro são exibidos corretamente
+    }
+}
